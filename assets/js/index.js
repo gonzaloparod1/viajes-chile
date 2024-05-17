e3d8aa9d041742a68f495373d096c49e95f62a3e
gonzaloparod1 <gonzalo.parodi@gmail.com>
2024-05-16 20:55:48 -0400

    add alert tooltip smoth scrolling

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+$("#enviarMensaje").click(function() {
+    alert("El mensaje fue enviado correctamente...");
+});
+$(function() {
+    $("#enviarMensaje").tooltip();
+});
+$(document).ready(function() {
+    $("a").on('click', function(event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
